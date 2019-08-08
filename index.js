9f1e2a09c9841c2494e3be100706c9b7f4ca18a9
github username <jpsypek@gmail.com>
2019-08-08 12:42:36 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+const findMinAndRemoveSorted = (array) => {
+  let min = array[0]
+  let i = 0
+  array.forEach((num, index) => {
+    if (num < min) {
+      min = num
+      i = index
+    }
+  })
+  array.splice(i,1)
+  return min
+}
 
+const merge = (array1, array2) => {
+  const newArray = []
+  while(array1.length !== 0 && array2.length !== 0) {
+    if (array1[0] < array2[0]) {
+      newArray.push(findMinAndRemoveSorted(array1))
+    } else {
+      newArray.push(findMinAndRemoveSorted(array2))
+    }
+  }
+  return newArray.concat(array1, array2)
+}
+
+const mergeSort = (array) => {
+  let midpoint = array.length/2
+  let firstHalf = array.slice(0, midpoint)
+  let secondHalf = array.slice(midpoint, array.length)
+  let sorted
+
+  if (array.length === 1) {
+    return array
+  } else {
+    sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
+  return sorted
+}
